c656e622aa10f291e8bd51b04358af13d922a4dc
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-02-02 15:49:44 +0500

    css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import Header from './components/Header';
-
+import Header from "./components/Header";
+import "./assets/css/main.css";
 function App() {
   return (
-    <div >
+    <div>
       <Header />
     </div>
   );
