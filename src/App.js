bc48704dcb2123867b7782d92b896d428e49dec4
Shuja Siddiqui <shuja0094@gmail.com>
2023-02-03 19:09:31 +0500

    footer update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./assets/css/main.css";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
 import Sidenav from "./components/Sidenav";
+import Footer from "./components/Footer";
+import AboutUs from "./components/AboutUs";
 function App() {
   return (
     <div>
       <Header />
       <Sidenav />
       <Banner />
+      <AboutUs />
       <Footer />
     </div>
   );
