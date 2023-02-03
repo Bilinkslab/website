564e258a173ea7b95ae3042adf5f0f86cee69acd
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-02-03 17:22:38 +0500

    footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Banner from "./components/Banner";
 import "./assets/css/main.css";
 import Header from "./components/Header";
 import Sidenav from "./components/Sidenav";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
       <Header />
       <Sidenav />
       <Banner />
+      <Footer />
     </div>
   );
 }
